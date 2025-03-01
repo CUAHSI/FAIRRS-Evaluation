a5e1a1e456a6eadffa8702884bf2f78b3a06d18f
Abner Bogan <abnerbog@buffalo.edu>
2025-03-01 12:16:55 -0500

    Add `codemeticulous` codemeta validation step

--- evaluators/fair4rs_comses/main.py
+++ evaluators/fair4rs_comses/main.py
@@ -1,31 +1,14 @@
 import json
-import argparse
 from services.evaluation_manager import FAIR4RS_Evaluation_Manager
-
-def load_metadata(file_path: str):
-    """
-    Loads the Codemeta JSON file.
-    """
-    try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            metadata = json.load(file)
-        print("Loaded Codemeta file.")
-        return metadata
-    except Exception as e:
-        print(f"Error loading Codemeta file: {e}")
-        exit(1)
 
 def main(codemeta_file):
     """
     Main function to run the FAIR4RS evaluation.
     """
 
-    # Load metadata
-    metadata = load_metadata(codemeta_file)
-
     # Run evaluation
     print("Running FAIR4RS evaluation...")
-    evaluation_manager = FAIR4RS_Evaluation_Manager(metadata)
+    evaluation_manager = FAIR4RS_Evaluation_Manager(codemeta_file)
     results = evaluation_manager.run_evaluation()
 
     # Print results
@@ -37,4 +20,5 @@ def main(codemeta_file):
 if __name__ == "__main__":
 
     codemeta_filepath = 'schemas/examples/HydroTrend_codemeta.json'
+    # codemeta_filepath = 'schemas/examples/chime.json'
     res = main(codemeta_filepath)
--- evaluators/fair4rs_comses/services/evaluation_manager.py
+++ evaluators/fair4rs_comses/services/evaluation_manager.py
@@ -1,31 +1,51 @@
-# Import validators & evaluators
-from schemas.codemeta_models import CodemetaMetadata  # Codemeta validation
+# Import evaluators
 from evaluators.findability.F1_evaluator import F1_evaluator # FAIR evaluator
+# Import libraries for loading codemeta file and running codemeticulous validator
+from codemeticulous.cli import load_and_create_model
+from codemeticulous.convert import STANDARDS
 # Import type hints
 from typing import Dict, Any
+import sys
+
 
 class FAIR4RS_Evaluation_Manager:
     """
     Manages and runs FAIR4RS evaluations.
     """
 
-    def __init__(self, metadata: Dict[str, Any]):
+    def __init__(self, codemeta_file: str, validate: bool = True):
         """
-        Initializes the evaluation manager and validates metadata before running evaluations.
+        Initializes the evaluation manager and loads/validates metadata before running evaluations.
         """
 
-        # # skipping this for now
-        # # Validate Codemeta metadata (raises an error if invalid)
-        # self.validated_metadata = CodemetaMetadata(**metadata) 
-        
-        self.metadata = metadata
-        
+        self.validate = validate
+        self.codemeta_file = codemeta_file
+
+        # run codemeticulous validation and return metadata
+        self.codemeta_metadata = self.run_codemeticulous_validation()
+
         # Use validated metadata for evaluation
         # Start with F1 for now
         self.evaluators = [
-            F1_evaluator(self.metadata)  # Ensure metadata is passed in correct format
+            F1_evaluator(self.codemeta_metadata) 
         ]
-    
+
+    def run_codemeticulous_validation(self):
+        """
+        Validates the Codemeta JSON file using `codemeticulous` directly in Python.
+        """
+
+        try:
+            model = STANDARDS['codemeta']["model"]  # Get Pydantic model for codemeta V3
+            codemeta_metadata = load_and_create_model(self.codemeta_file, model)  # Run validation
+            print(f"✅ {self.codemeta_file} is a valid codemeta file.")
+            # convert pydantic object to dict and return
+            return(codemeta_metadata.dict())
+        except ValueError as e:
+            # if invalid codemeta file show errors and break
+            print(f"❌ Codemeta validation failed:\n{e}")
+            sys.exit(1)
+
     def run_evaluation(self) -> Dict[str, Any]:
         """
         Runs all defined FAIR4RS evaluators and collects results.
