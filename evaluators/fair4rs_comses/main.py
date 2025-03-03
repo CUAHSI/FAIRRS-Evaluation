16a2fcbf0af3c2614e0d4c0e2d9ff2584d350ae4
Abner Bogan <abnerbog@buffalo.edu>
2025-03-03 09:37:37 -0500

    Update path to example codemeta file

--- evaluators/fair4rs_comses/main.py
+++ evaluators/fair4rs_comses/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from services.evaluation_manager import FAIR4RS_Evaluation_Manager
 
 def main(codemeta_file):
@@ -19,6 +20,13 @@ def main(codemeta_file):
 
 if __name__ == "__main__":
 
-    codemeta_filepath = 'schemas/examples/HydroTrend_codemeta.json'
-    # codemeta_filepath = 'schemas/examples/chime.json'
+    # Input codemeta file (JSON) under schema/examples
+    codemeta_filename = 'chime.json'
+
+    # Get the directory where main.py is located
+    main_script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the path to the example JSON file (may change location of schema folder later)
+    codemeta_filepath = os.path.normpath(os.path.join(main_script_dir, "..", "..", "schemas", "examples", codemeta_filename))
+
     res = main(codemeta_filepath)
