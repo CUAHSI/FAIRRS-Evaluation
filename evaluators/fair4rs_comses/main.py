cc64d3fd6710177c22140a3fa3d80026944acf24
Abner Bogan <abnerbog@buffalo.edu>
2025-03-17 14:48:58 -0400

    Modify `main.py` for updated workflow

--- evaluators/fair4rs_comses/main.py
+++ evaluators/fair4rs_comses/main.py
@@ -1,27 +1,48 @@
 import json
 import os
-from services.evaluation_manager import FAIR4RS_Evaluation_Manager
+from evaluators import MyEvaluator
+from utils.crosswalk_loader import CrosswalkLoader
 
 def main(codemeta_file):
-    """
-    Main function to run the FAIR4RS evaluation.
-    """
 
-    # Run evaluation
-    print("Running FAIR4RS evaluation...")
-    evaluation_manager = FAIR4RS_Evaluation_Manager(codemeta_file)
-    results = evaluation_manager.run_evaluation()
+    # load evaluator and load/validate codemeta file
+    evaluator = MyEvaluator(codemeta_file)
+    codemeta_json = evaluator.validate_codemeta_file()
+    
+    # load evaluator methods crosswalk
+    crosswalk = CrosswalkLoader(codemeta_json)
+    eval_methods = crosswalk.map_evaluators_to_codemeta()
 
-    # Print results
-    print("FAIR4RS Evaluation Results:")
-    print(json.dumps(results, indent=4))
+    # initialize dictionary of results  
+    results = {}
+
+    # Get only method names that contain 'eval'
+    eval_method_names = [
+    name for name in dir(evaluator)
+    if "eval" in name and callable(getattr(evaluator, name))
+]
+
+    # run evalutions for each evaluator
+    for method_name in eval_method_names:
+        # extract evaluation method from evaluator class
+        method = getattr(evaluator, method_name, None)
+        # run evaluation and store/print result
+        try:
+            result = method(**eval_methods[method_name])
+        # some evaluation methods need codemeta properties in crosswalk
+        except KeyError:
+            result = "Need codemeta property in crosswalk."
+
+        results[method_name] = result
+        print(f"{method_name} result: {result}")
 
     return results
+
 
 if __name__ == "__main__":
 
     # Input codemeta file (JSON) under schema/examples
-    codemeta_filename = 'chime.json'
+    codemeta_filename = 'HydroTrend_codemeta.json'
 
     # Get the directory where main.py is located
     main_script_dir = os.path.dirname(os.path.abspath(__file__))
