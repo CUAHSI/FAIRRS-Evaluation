a57fd2b89710d6500e7fab78dfce97ac6fd194e0
Abner Bogan <abnerbog@buffalo.edu>
2025-10-20 14:12:07 -0400

    Update to query API for model programs and extract metadata via hsclient

--- schemas/hydroshare/utils/query_hydroshare_repository.py
+++ schemas/hydroshare/utils/query_hydroshare_repository.py
@@ -1,21 +1,22 @@
 import os
-import re
 import json
 import requests
 from typing import List, Dict
+from hsclient import HydroShare
+from hsmodels.schemas.aggregations import ModelProgramMetadata
 
-def get_model_instances() -> List[Dict]:
+def get_model_programs() -> List[Dict]:
     """
-    Queries HydroShare API for metadata from all model instance resources.
+    Queries HydroShare API for metadata from all resources tagged as model programs.
     """
 
-    model_instances = []
+    model_programs = []
     page_num = 1
     previous_resources = None
 
     while True:
         response = requests.get(
-            'https://hydroshare.org/discoverapi/?filter={%22type%22:[%22Model%20Instance%22]}',
+            'https://hydroshare.org/discoverapi/?filter={%22type%22:[%22Model%20Program%22]}',
             params={'pnum': page_num}
         )
         json_response = response.json()
@@ -24,13 +25,39 @@ def get_model_instances() -> List[Dict]:
         if resources == previous_resources:
             break
 
-        model_instances.extend(resources)
+        model_programs.extend(resources)
         previous_resources = resources
         page_num += 1
 
-    return model_instances
+    return model_programs
 
-def write_to_file(resources,output_folder):
+def get_model_program_metadata(hs_instance,resource):
+    """
+    Get model program metadata through hsclient package.
+    """
+    
+    hs_res_id = resource['short_id']
+    hs_res = hs_instance.resource(hs_res_id)
+
+    # initialize metadata fields
+    resource['version'] = []
+    resource['programming_languages'] = []
+    resource['operating_systems'] = []
+    resource['code_repository'] = []
+    resource['rights'] = []
+
+    for agg in hs_res.aggregations():
+        metadata = agg._retrieved_metadata
+        if isinstance(metadata, ModelProgramMetadata):
+            resource['version'] += metadata.version
+            resource['programming_languages'] += metadata.programming_languages
+            resource['operating_systems'] += metadata.operating_systems
+            resource['code_repository'] += metadata.code_repository
+            resource['rights'] += metadata.rights.url
+
+    return resource     
+
+def write_to_file(hs_instance,resources,output_folder):
     """
     Writes HydroShare resource metadata to file.
     """
@@ -39,6 +66,10 @@ def write_to_file(resources,output_folder):
     os.makedirs(output_dir, exist_ok=True)
 
     for resource in resources:
+
+        # add file level metadata
+        resource = get_model_program_metadata(hs_instance,resource)
+
         # use resource ID as file name
         filename = os.path.join(output_dir, f"{resource['short_id']}.json")
         with open(filename, "w", encoding="utf-8") as f:
@@ -47,9 +78,13 @@ def write_to_file(resources,output_folder):
 
 def main():
 
-    # fetch model instances and write to file
-    model_instances = get_model_instances()
-    write_to_file(model_instances,'model_instances_metadata')
+    hs = HydroShare()
+    hs.sign_in()
+
+    # fetch model program resources and write to file
+    model_programs_ids = get_model_programs()
+    # extract metadata from model programs resource ids and write to file
+    write_to_file(hs,model_programs_ids ,'model_program_metadata')
 
 if __name__ == "__main__":
     main()
