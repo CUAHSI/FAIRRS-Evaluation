e3a43047bce700d87452d51ea8b26bb517ef3104
Abner Bogan <abnerbog@buffalo.edu>
2025-10-21 10:55:14 -0400

    Update utility script for extracting metadata from model programs

--- schemas/hydroshare/utils/query_hydroshare_repository.py
+++ schemas/hydroshare/utils/query_hydroshare_repository.py
@@ -4,6 +4,7 @@ import requests
 from typing import List, Dict
 from hsclient import HydroShare
 from hsmodels.schemas.aggregations import ModelProgramMetadata
+from pydantic import ValidationError
 
 def get_model_programs() -> List[Dict]:
     """
@@ -31,31 +32,43 @@ def get_model_programs() -> List[Dict]:
 
     return model_programs
 
-def get_model_program_metadata(hs_instance,resource):
+def get_model_program_metadata(hs_instance,resource_id):
     """
     Get model program metadata through hsclient package.
     """
-    
-    hs_res_id = resource['short_id']
-    hs_res = hs_instance.resource(hs_res_id)
+
+
 
     # initialize metadata fields
-    resource['version'] = []
-    resource['programming_languages'] = []
-    resource['operating_systems'] = []
-    resource['code_repository'] = []
-    resource['rights'] = []
+    metadata = {}
 
-    for agg in hs_res.aggregations():
-        metadata = agg._retrieved_metadata
-        if isinstance(metadata, ModelProgramMetadata):
-            resource['version'] += metadata.version
-            resource['programming_languages'] += metadata.programming_languages
-            resource['operating_systems'] += metadata.operating_systems
-            resource['code_repository'] += metadata.code_repository
-            resource['rights'] += str(metadata.rights.url)
+    while True:
+        try: 
+            hs_res = hs_instance.resource(resource_id)
+        except Exception as e:  
+            # Check if the error message contains the specific 401 Unauthorized text
+            error_message = str(e)
+            if "status_code 401" in error_message:
+                print(f"Resource https://hydroshare.org/resource/{resource_id} is discoverable or private.")             
+            break
+        
+        try:
+            for agg in hs_res.aggregations():
+                agg_metadata = agg._retrieved_metadata
+                if isinstance(agg_metadata, ModelProgramMetadata):
+                    metadata['version'] = agg_metadata.version
+                    metadata['programming_languages'] = agg_metadata.programming_languages
+                    metadata['operating_systems'] = agg_metadata.operating_systems
+                    metadata['code_repository'] = str(agg_metadata.code_repository)
+                    metadata['rights'] = str(agg_metadata.rights.url)
+                    metadata['software_type'] = agg_metadata.title
+                    break #TODO: add handling if there are multiple model programs in a resource
+        except ValidationError as e:
+            print(f"Error accessing resource https://hydroshare.org/resource/{resource_id}: {e}")
 
-    return resource     
+        break
+
+    return metadata    
 
 def write_to_file(hs_instance,resources,output_folder):
     """
@@ -66,15 +79,27 @@ def write_to_file(hs_instance,resources,output_folder):
     os.makedirs(output_dir, exist_ok=True)
 
     for resource in resources:
+        # use the resource id as input to hsclient and for output file naming
+        resource_id = resource['short_id']
 
         # add file level metadata
-        resource = get_model_program_metadata(hs_instance,resource)
+        metadata = get_model_program_metadata(hs_instance,resource_id)
 
-        # use resource ID as file name
-        filename = os.path.join(output_dir, f"{resource['short_id']}.json")
-        with open(filename, "w", encoding="utf-8") as f:
-            json.dump(resource, f, indent=2, ensure_ascii=False)
-        print(f"Saved to file: {filename}")
+        # write metadata to file if resource contains model program
+        if metadata: 
+            # add resource level metadata 
+            metadata['title'] = resource['title']
+            metadata['availability'] = resource['availability']
+            metadata['authors'] = resource['authors']
+            metadata['subject'] = resource['subject']
+            metadata['created'] = resource['created']
+            metadata['modified'] = resource['modified']
+
+            # use resource ID as file name
+            filename = os.path.join(output_dir, f"{resource_id}.json")
+            with open(filename, "w", encoding="utf-8") as f:
+                json.dump(metadata, f, indent=2, ensure_ascii=False)
+            print(f"Saved to file: {filename}")
 
 def main():
 
